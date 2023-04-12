9d56fc8e0ea6838281c61f41548de41a049fd30d
Fenway Powers <31604245+fenwaypowers@users.noreply.github.com>
2023-04-11 20:27:53 -0400

    formatting fixes and finishing touches

--- README.md
+++ README.md
@@ -1,45 +1,31 @@
 # yta-dlp
 Download albums in opus format from youtube with metadata.
 
-Yes, I'm aware that this script is extremely scuffed. There are many issues with this script, and they will be talked about later in this README.
-If you feel like the script can be improved, please fork this repository and improve it. I know it needs improving.
-
-This was made for albums, not playlists.
-
 ## Prerequisites
 * [Python 3](https://www.python.org/)
 * [yt-dlp](https://github.com/yt-dlp/yt-dlp)
-* [requests](https://pypi.org/project/requests/)
-* [validators](https://pypi.org/project/validators/)
-* [mutagen](https://pypi.org/project/mutagen/)
-* [html.parser](https://docs.python.org/3/library/html.parser.html#module-html.parser)
 * [ytmusicapi](https://ytmusicapi.readthedocs.io/en/stable/)
+* [mutagen](https://pypi.org/project/mutagen/)
+
+## Getting Started
+
+run `python3 -m pip install -r requirements.txt`
 
 ## Usage
 To run, you can simply execute `python3 yta-dlp.py`.
 
-You must supply a playlist link to an album from youtube music.
-
 Example Use:
-`python3 yta-dlp.py https://music.youtube.com/playlist?list=OLAK5uy_n0vSV9smWbw8O4q_rHkjWWoE5yshlPJU4`
+`python3 yta-dlp.py -o path/to/albums -e opus`
 
 ## How It Works
-* yt-dlp downloads the album in a temporary folder in opus format.
-* Each file is searched in ytmusicapi from its youtube id.
-* If metadata is found, it is applied to the file.
-* Sometimes ytmusicapi returns an incorrect search, and sometimes it won't even return a search.
-* In these cases, metadata from the previous song will be used. The title of the song will be taken from the filename.
-* If the first song has no metadata, it will get metadata later at the very end during the final check.
-* If there are 2 artists on the album, they will both appear in the `album_artist` field.
-* If there are more than 2 artists on the album, the `album_artist` field will be set to "Various Artists".
+* user searches for an album using ytmusicapi `search()`
+* user selects an album, metdata of the album saved from ytmusicapi `get_album()`
+* yt-dlp downloads the album, and then applies the metadata
 
 ## Known Errors
-* For songs that have no metadata available, their `artist` field will automatically be set to whatever the `album_artist` field is. Yes, this means that albums with "Various Artists" as the `album_artist` could potentially have songs with "Various Artists" as their `artist` field. Just the way it is, unfortunately. you can always edit the metadata with a tool such as [mp3tag](https://www.mp3tag.de/en/).
-* If you get this error : `mutagen.ogg.error: read b'\xff\xd8\xff\xe0', expected b'OggS', at 0x0`, just try again.
-* Obviously if you put in anything other than a playlist link to an album, you will get wonky results. This was made for albums, not playlists.
+* Albums that are by "Various Artists" won't have correct metadata generated. The individual artists won't appear in the `artist` metadata field. This is because the only data I was able to get from ytmusicapi had "Various Artists" as the only artist.
 
 ## Future Features
-DISCLAIMER: This program currently does pretty much everything I want it to, leaving me little reason to improve it. These features are things I would like but don't need. You will probably get these features faster if you implemenet them yourself. If a ton of people flock to this repo for some reason, then yeah sure I'll work on it again. I'll probably rewrite the entire thing in that case, since it is very spaghetti as of now.
-
 * Support for other audio formats such as AAC
-* Download image album (and also make it an option to put it in the metadata)
+* Support for downloading individual songs with metadata
+* Support for "Various Artists" albums
--- yta-dlp.py
+++ yta-dlp.py
@@ -1,23 +1,15 @@
-import subprocess
-import os
-from mutagen.oggopus import OggOpus
-from mutagen.flac import Picture
-from mutagen.aac import AAC
-from mutagen import File
-from mutagen.easyid3 import EasyID3
-from mutagen.id3 import ID3, ID3NoHeaderError
-from html.parser import HTMLParser
-from html.entities import name2codepoint
-from ytmusicapi import YTMusic
-import validators
-import yt_dlp
-
-from pydub import AudioSegment
-
-
 import urllib.request
-import os, shutil, argparse, logging, sys, subprocess, requests
-from zipfile import ZipFile
+import os
+import shutil
+import argparse
+import logging
+import sys
+import yt_dlp
+import string
+import tempfile
+from mutagen.oggopus import OggOpus
+from mutagen.aac import AAC
+from ytmusicapi import YTMusic
 
 logging.basicConfig(format='%(message)s', stream=sys.stdout, level=logging.INFO)
 
@@ -27,25 +19,28 @@ def main():
     global temp_dir
     global yt
     global format
+    global extension
+    global output_dir
 
     yt = YTMusic()
 
-    temp_dir = "yta-dlp-temp"
+    temp_dir = tempfile.mkdtemp()
     
     parser = argparse.ArgumentParser(description='Download Albums from Youtube with metadata.')
-    parser.add_argument('--link', help='Youtube Music link for an album or a song.', default=None)
-    parser.add_argument(
-        '--manual', help='Input the metadata for the album manually. Not implemented', action='store_true')
-    parser.add_argument(
-        '--resume', help='If there was a previous error, but your content was downloaded, you can resume the metadata process. Not implemented.', action='store_true')
+    parser.add_argument('-o', '--output-path', help="specify a path where you want your files to go.", default="")
+    parser.add_argument('-e', '--extension', help="specify the output extension of the files.", choices = ["ogg", "opus"], default="ogg")
 
     args = parser.parse_args()
 
-    link = args.link
     format = "opus"
-    
-    if (os.path.isdir(temp_dir)) == False:
-        os.mkdir(temp_dir)
+    output_dir = os.path.abspath(args.output_path)  # Convert to an absolute path
+
+    # Create the output directory if it doesn't exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    extension = args.extension
+
 
     album_data = search_album()
 
@@ -53,27 +48,13 @@ def main():
 
     album_metadata = get_album_metadata(album_data)
 
-    with open("album_metadata.txt", "w") as myfile:
-        myfile.write(str(album_metadata))
-
     link = album_metadata['audioPlaylistId']
-
-
-    print(link)
 
     download(link, format)
 
     apply_metadata(album_metadata)
 
-    '''
-    album_list, metadata_dict = fetch_metadata()
-
-    album_name = choose_album(album_list)
-
-    global_metadata = gather_global_data(album_name, metadata_dict)
-
-    print(global_metadata)
-    '''
+    finish(album_metadata)
 
 def search_album():
     while(True):
@@ -82,7 +63,7 @@ def search_album():
         if search_term == "q":
             sys_exit(0)
 
-        search = yt.search(search_term, filter="albums", limit=10)
+        search = yt.search(search_term, filter="albums")
 
         albums_list = []
         album_data_list = []
@@ -114,29 +95,21 @@ def search_album():
 
     album_data = album_data_list[album_selected]
 
-    with open("test_data2.txt", "w") as myfile:
-        myfile.write(str(album_data))
-
     return album_data
 
 def download_cover(album_data: str):
     try:
         num_imgs = len(album_data['thumbnails'])
-        filename = temp_dir + "/" + "folder.jpg"
+        filename = os.path.join(temp_dir, "folder.jpg")
         urllib.request.urlretrieve(
             album_data['thumbnails'][num_imgs - 1]['url'], filename)
     except:
-        print("Failed to download album cover.")
+        logging.info("Failed to download album cover.")
 
 def get_album_metadata(album_data : str):
 
-    print(album_data)
-
     data = yt.get_album(album_data['browseId'])
     return data
-
-    #print("fail")
-    #sys_exit(1)
 
 def download(url : str, format : str):
     format_ids = {"opus" : "251",
@@ -147,7 +120,7 @@ def download(url : str, format : str):
                   "mp4" : "140"}
 
     if format not in format_ids:
-        sys_exit(1)
+        sys_exit(1, f"Cannot account for format: {format}")
 
     ydl_opts = {
         'format': format_ids[format],
@@ -167,15 +140,16 @@ def download(url : str, format : str):
             format = "m4a"
 
         for song in os.listdir(temp_dir):
-            song_path = temp_dir + "/" + song
+            song_path = os.path.join(temp_dir, song)
+            
 
             if song.endswith("]"):
                 os.rename(song_path, song_path + "." + format)
             if song.endswith(".opus"):
-                os.rename(song_path, song_path.split(".opus")[0] + ".ogg")
+                os.rename(song_path, song_path.split("opus")[0] + extension)
     
     except:
-        sys_exit(1)
+        sys_exit(1, f"Problem occured downloading from url: {url}")
 
 def apply_metadata(album_metadata):
     tracks = album_metadata['tracks']
@@ -187,9 +161,9 @@ def apply_metadata(album_metadata):
         for file in os.listdir(temp_dir):
             if file.startswith(file_startswith_val) or file.startswith("0" + file_startswith_val):
                 if format == "aac":
-                    f = AAC(temp_dir + "/" + file)
+                    f = AAC(os.path.join(temp_dir, file))
                 else:
-                    f = OggOpus(temp_dir + "/" + file)
+                    f = OggOpus(os.path.join(temp_dir, file))
 
                 f["title"] = tracks[i]['title']
                 f["album"] = tracks[i]['album']
@@ -214,49 +188,6 @@ def artists_name(input_artists: list, purpose="album"):
             names.append(artist['name'])
         return ", ".join(names[:-1]) + " & " + names[-1]
 
-def fetch_metadata():
-
-    metadata_dict = {}
-    album_list = {}
-
-    print("Getting metadata...")
-
-    for file in os.listdir(temp_dir):
-
-        id = parse_id(file)
-
-        data = yt.search(id)
-
-        '''
-        try:
-            print(data[0]['videoId'])
-        except:
-            print("no data found")
-        '''
-
-        try:
-            album_name = data[0]["album"]["name"]
-            if album_name in album_list:
-                album_list[album_name] += 1
-            else:
-                album_list[album_name] = 1
-        except:
-            pass
-
-        metadata_dict[id] = data
-
-    print(album_list)
-
-    return album_list, metadata_dict
-
-def choose_album(album_list: dict):
-    print_list = []
-    for i in album_list:
-        print(i)
-        print_list.append(i)
-
-    return select(print_list, "Albums")
-    
 def select(inputlist : list, selectiontype: str):
     for j, name in enumerate(inputlist):
         print(f"[{j + 1}] {name}")
@@ -276,282 +207,32 @@ def select(inputlist : list, selectiontype: str):
 
     selection = int(selection) - 1
 
-    print("Selection made: " + inputlist[selection])
+    logging.info("Selection made: " + inputlist[selection])
     return selection
 
-def gather_global_data(album_name: str, metadata_dict: dict):
-    year_list = []
-    artists_list = []
+def finish(album_metadata):
+    output_folder = artists_name(album_metadata['artists']) + " - " + album_metadata['title'] + " (" + album_metadata['year'] + ")"
+    output_folder = sanitize_filename(output_folder)
 
-    album_data = ""
+    if not os.path.exists(os.path.join(output_dir, output_folder)):
+        os.mkdir(os.path.join(output_dir, output_folder))
 
-    for i in metadata_dict:
-        data = metadata_dict[i]
+    for i in os.listdir(temp_dir):
+        shutil.copy(os.path.join(temp_dir, i), os.path.join(output_dir, output_folder, i))
 
-        try:
-            if data[0]["album"]["name"] == album_name:
-                album_data = yt.get_album(data[0]['album']['id'])
-                print(data[0]['album']['id'])
-
-                album_year = album_data['year']
-                if album_year not in year_list:
-                    year_list.append(album_year)
-
-                artist_name = ""
-                for j in range(0, len(data[0]["artists"])-1):
-                    artist_name += data[0]["artists"][j]["name"] + ", "
-                artist_name += data[0]["artists"][len(data[0]["artists"])-1]["name"]
-
-                if artist_name not in artists_list:
-                    artists_list.append(artist_name)
-        except:
-            pass
-
-    year = select(year_list)
-    artist = select(artists_list)
-
-    return {"album_name" : album_name,
-            "album_year" : year,
-            "album_artist" : artist}
-
-def sys_exit(code: int):
-    '''
     for file in os.listdir(temp_dir):
-        os.remove(temp_dir + "/" + file)
-
+        os.remove(os.path.join(temp_dir, file))
     os.rmdir(temp_dir)
-    '''
 
+def sanitize_filename(filename):
+    valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+    cleaned_filename = ''.join(c for c in filename if c in valid_chars)
+    return cleaned_filename
+
+def sys_exit(code: int, message=""):
+    if message != "":
+        logging.info(message)
     sys.exit(code)
-
-'''
-
-
-yt = YTMusic("oauth.json")
-
-ver = "b0.3"
-
-temp_dir = ".yta-dlp-temp"
-
-if (os.path.isdir(temp_dir)):
-    for file in os.listdir(temp_dir):
-        os.remove(temp_dir + "/" + file)
-
-if (os.path.isdir(temp_dir)) == False:
-    os.mkdir(temp_dir)
-
-def title_from_filename(instr:str):
-    return_string = ""
-    insplit = instr.split(". ")
-    for i in range(1, len(insplit)):
-        if i < 2:
-            return_string += insplit[i]
-        else:
-            return_string += ". " + insplit[i]
-    return return_string.split("[")[0]
-
-
-def sys_exit():
-    for file in os.listdir(temp_dir):
-        os.remove(temp_dir + "/" + file)
-
-    os.rmdir(temp_dir)
-
-    sys.exit(0)
-
-
-def toValidFileName(input: str):
-    input_split = []
-    output_dir = input
-    for char in ["<", ">", ":", "\"", "/", "\\", "|", "?", "*"]:
-        if char in output_dir:
-            dir_split = output_dir.split(char)
-            output_dir = ""
-            for x in dir_split:
-                output_dir += x
-    return output_dir
-
-
-def help_message():
-    print(
-Welcome to yta-dlp beta 0.3!
-    
-To run, you can simply execute `python3 yta-dlp.py`.
-
-You must supply a playlist link to an album from youtube music.
-
-Example Use:
-`python3 yta-dlp.py https://music.youtube.com/playlist?list=OLAK5uy_n0vSV9smWbw8O4q_rHkjWWoE5yshlPJU4`
-)
-    sys_exit()
-
-mode = "stream"
-link = ""
-
-if len(sys.argv) > 1:
-    for j in range(1, len(sys.argv)):
-        if "." in sys.argv[j]:
-            testlink = ""
-
-            if "http://" in sys.argv[j]:
-                testlink += "https://" + sys.argv[j][7:]
-            elif "https://" not in sys.argv[j]:
-                testlink += "https://" + sys.argv[j]
-            else:
-                testlink += sys.argv[j]
-
-            if validators.url(testlink) and "youtu" in testlink:
-                if "&feature=share" in testlink:
-                    testlink = testlink.split("&feature=share")[0]
-                link = testlink
-                search = False
-            else:
-                print(testlink)
-                print("Error! Invalid link provided.")
-                sys_exit()
-        for m in ["-h", "h", "help", "-help"]:
-            if m == sys.argv[j]:
-                help_message()
-else:
-    help_message()
-
-ytdlpcommand = ["yt-dlp", "--extract-audio", "--embed-thumbnail", "-o", temp_dir + "/%(playlist_index)s. %(title)s [%(id)s].ogg", link]
-
-#print(ytdlpcommand)
-subprocess.run(ytdlpcommand)
-
-print()
-
-name = ""
-folder = temp_dir
-data = []
-continue_ = True
-album_name = "album_name"
-artist_name = "artist_name"
-artists_list = []
-
-for i in os.listdir(folder):
-
-    continue_ = True
-
-    print(i)
-
-    old_data = data
-
-    data = yt.search(i[-17:-6])
-
-    print("--------------------------------------------------------------------")
-
-    if len(data) == 0:
-        print("Song with id " + i[-17:-6] + " will not have metadata generated right now. Sorry about that...\nWill try to retrieve metadata for it later.")
-        continue_ = False
-    else:
-        if data[0]['videoId'] != i[-17:-6]:
-            print("ERROR: Innacurate search for song with id " +
-                    i[-17:-6] + ", instead got video of id " + data[0]['videoId'] + ".")
-            if old_data != []:
-                print("Working around this by using the previous song's metadata.")
-                data = old_data
-                data[0]["title"] = title_from_filename(i)
-            else:
-                print("Song with id " +
-                        i[-17:-6] + " will not have metadata generated right now. Sorry about that...\nWill try to retrieve metadata for it later.")
-                continue_ = False
-        else:
-            print("Valid metadata found!")
-    print("--------------------------------------------------------------------")
-
-    if continue_:
-
-        album_data = yt.get_album(data[0]['album']['id'])
-
-        song_date = album_data['year']
-        track_num = str(int(i.split(".")[0]))
-        album_name = data[0]["album"]["name"]
-        song_name = data[0]["title"]
-
-        artist_name = ""
-        for j in range(0, len(data[0]["artists"])-1):
-            artist_name += data[0]["artists"][j]["name"] + ", "
-        artist_name += data[0]["artists"][len(data[0]["artists"])-1]["name"]
-
-        if artist_name not in artists_list:
-            artists_list.append(artist_name)
-
-        f = OggOpus(folder + "/" + i)
-        f["title"] = song_name
-        f["album"] = album_name
-        f["artist"] = artist_name
-        f["tracknumber"] = track_num
-        f["albumartist"] = artist_name
-        f["date"] = song_date
-        f.save()
-
-    print()
-
-if len(artists_list) == 2:
-    artist_name = artists_list[0] + " and " + artists_list[1]
-elif len(artists_list) > 2:
-    artist_name = "Various Artists"
-
-destination = toValidFileName(artist_name + " - " + album_name)
-
-if (os.path.isdir(destination)) == False:
-    os.mkdir(destination)
-
-for song in os.listdir(folder):
-    src = os.path.join(folder, song)
-    dst = os.path.join(destination, song)
-    shutil.move(src, dst)
-
-album_list = []
-date_list = []
-
-for j in os.listdir(destination):
-    fileopen = OggOpus(destination + "/" + j)
-    fileopen["albumartist"] = artist_name
-
-    if "album" in fileopen:
-        if fileopen["album"][0] not in album_list:
-            album_list.append(fileopen["album"][0])
-    
-    if "date" in fileopen:
-        if fileopen["date"][0] not in date_list:
-            date_list.append(fileopen["date"][0])
-
-    if "title" not in fileopen:
-        song_name = title_from_filename(j)
-        fileopen["title"] = song_name
-    
-    if "artist" not in fileopen:
-        fileopen["artist"] = artist_name
-    
-    if "tracknumber" not in fileopen:
-        fileopen["tracknumber"] = str(int(j.split(".")[0]))
-        
-
-    fileopen.save()
-
-if len(album_list) > 1:
-    print("ERROR: Have multiple entries for album name! Using the first one in the list.")
-
-if (len(date_list)) > 1:
-    print("ERROR: Have multiple entries for album date! Using the first one in the list.")
-
-for j in os.listdir(destination):
-    fileopen = OggOpus(destination + "/" + j)
-    fileopen["albumartist"] = artist_name
-
-    if len(album_list) > 0:
-        fileopen["album"] = album_list[0]
-
-    if len(date_list) > 0:
-        fileopen["date"] = date_list[0]
-
-    fileopen.save()
-
-sys_exit()
-'''
 
 if __name__ == "__main__":
     main()
